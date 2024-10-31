b5f032acdea8db47048c6c33b02a491a706e2dfa
aruquipadalid <aruquipadalid@gmail.com>
2024-10-31 01:16:09 -0400

    Update index.py

--- desafio3_taylor/Contenido/index.py
+++ desafio3_taylor/Contenido/index.py
@@ -1,1 +1,39 @@
+from math import exp, factorial
 
+
+# Función a aproximar
+def f(x):
+    return exp(2 * x)
+
+
+# Derivada de orden n de la función
+def df(x, order=1):
+    return 2**order * exp(2 * x)
+
+
+# Función generadora de aproximaciones con serie de Taylor
+def aprox(x_0, x_1):
+    iteration = 1
+    distance = abs(x_1 - x_0)
+    error = df(x_0) * distance
+    value = f(x_0) + error
+    yield iteration, value, error
+    while True:
+        iteration += 1
+        error = (df(x_0, iteration) * pow(distance, iteration)) / factorial(iteration)
+        value += error
+        yield iteration, value, error
+
+
+tolerance = 0.001
+x_0 = 1
+print(f"Se conoce el valor x_0 = {x_0} | f(x_0) = {f(x_0)}")
+x_1 = float(input("Ingrese el valor de x_1: "))
+
+# Iterar en el generador hasta que el error sea menor a la tolerancia
+for iteration, value, error in aprox(x_0, x_1):
+    if error < tolerance:
+        break
+    print(f"Iteración: {iteration}, Valor: {value:4f}, Error: {error:4f}")
+
+print(f"Valor real de x_1 | {f(x_1)}")
